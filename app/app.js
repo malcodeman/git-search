cd733a201e711e46b765334308b28a971705c3f0
malcodeman <26983729+malcodeman@users.noreply.github.com>
2018-01-26 11:57:13 +0100

    Basic setup.

---
+++ app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+angular.module('shiffAdmin', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({
+    redirectTo: '/view1'
+  });
+}]);
